b2bf2834559b262cc57d5302d60d51b32f9bdbec
4mmang <ammangdeveloper@gmail.com>
2024-11-03 09:31:44 +0800

    [fix] callback redirect google

--- config/services.php
+++ config/services.php
@@ -33,6 +33,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'https://suruhkoding.web.id/auth/google/callback',
+        'redirect' => 'https://datau.suruhkoding.web.id/auth/google/callback',
     ],
 ];
