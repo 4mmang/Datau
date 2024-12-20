fdfb780e884b516f3253356056b44b8bed65089d
4mmang <ammangdeveloper@gmail.com>
2024-12-20 14:29:59 +0800

    [fix] perbaikan tampilan detail artikel

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,19 +1,8 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Artcile</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
-</head>
-
-<body>
-    <div class="container mt-4">
-        <p class="fs-3"><a href="{{ url('/') }}"><i class="fas fa-arrow-left fs-4 me-2" style="color: #38527E"></i></a>{{ $article->title }}</p>
+@extends('layouts.app')
+@section('content')
+    <div class="container" style="margin-top: 9rem">
+        <p class="fs-3"><a href="{{ url('/') }}"><i class="fas fa-arrow-left fs-4 me-2"
+                    style="color: #38527E"></i></a>{{ $article->title }}</p>
         <div class="col-md-4">
             <img src="{{ asset('storage/' . $article->cover) }}" class="img-fluid" alt="">
         </div>
@@ -21,9 +10,4 @@
             {!! $article->description !!}
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endsection
