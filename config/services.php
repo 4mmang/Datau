978dee9620f4ded5a5ae8f25d4c2b33cdfb0beba
4mmang <ammangdeveloper@gmail.com>
2025-01-06 09:50:31 +0800

    [fix] perbaikan login

--- config/services.php
+++ config/services.php
@@ -33,12 +33,12 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'https://datau.suruhkoding.web.id/auth/google/callback',
+        'redirect' => 'https://pusdata.pusatstudiaiunsulbar.com/auth/google/callback',
     ],
 
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'https://datau.suruhkoding.web.id/auth/github/callback',
+        'redirect' => 'https://pusdata.pusatstudiaiunsulbar.com/auth/github/callback',
     ],
 ];
